bbf40315d1991732fed4f253e06f0bd4dc6d3e73
ziyanoffl <86348725+ziyanoffl@users.noreply.github.com>
2023-07-29 15:49:21 +0530

    Made changes to terraform and deployment yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,39 +19,42 @@ resource "azurerm_resource_group" "devopsprojectb" {
   location = "East US"
 }
 
-resource "azurerm_container_registry" "devopsprojectb" {
-  name                = "devopsprojectb"
-  resource_group_name = azurerm_resource_group.devopsprojectb.name
-  location            = "East US"
-  sku                 = "Standard"
-  admin_enabled       = true
+resource "azurerm_container_registry" "appcr" {
+  name                    = "appcr"
+  resource_group_name     = azurerm_resource_group.devopsprojectb.name
+  location                = "East US"
+  sku                     = "Premium"
+  admin_enabled           = true
+  zone_redundancy_enabled = true
+  georeplications {
+    location                  = "eastasia"
+    regional_endpoint_enabled = true
+    tags                      = {}
+    zone_redundancy_enabled   = true
+  }
+
+  timeouts {}
 }
 
-# resource "azurerm_role_assignment" "devopsprojectc" {
-#   scope                = azurerm_container_registry.devopsprojectc.id
-#   role_definition_name = "acrpull"
-#   principal_id         = azurerm_app_service.devopsprojectc.identity[0].principal_id
-# }
-
-resource "azurerm_app_service_plan" "ASP-devopsprojectb-9277" {
-  name                = "ASP-devopsprojectb-9277"
+resource "azurerm_app_service_plan" "appservice" {
+  name                = "appservice"
   location            = "East US"
   kind                = "linux"
   reserved            = true
   resource_group_name = azurerm_resource_group.devopsprojectb.name
 
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Basic"
+    size = "B1"
   }
   timeouts {}
 }
 
-resource "azurerm_app_service" "devopsprojectapp" {
-  name                = "devopsprojectapp"
+resource "azurerm_app_service" "calculatorwebapp" {
+  name                = "calculatorwebapp"
   location            = "East US"
   resource_group_name = azurerm_resource_group.devopsprojectb.name
-  app_service_plan_id = azurerm_app_service_plan.ASP-devopsprojectb-9277.id
+  app_service_plan_id = azurerm_app_service_plan.appservice.id
 
   site_config {
     always_on = false
@@ -71,10 +74,10 @@ resource "azurerm_app_service" "devopsprojectapp" {
   https_only = true
 
   app_settings = {
-    DOCKER_REGISTRY_SERVER_URL          = "https://${azurerm_container_registry.devopsprojectb.name}.azurecr.io"
+    DOCKER_REGISTRY_SERVER_URL          = "https://${azurerm_container_registry.appcr.name}.azurecr.io"
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
-    DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.devopsprojectb.admin_username
-    DOCKER_REGISTRY_SERVER_PASSWORD     = "a60L7deektrSmbPwTIDHIBVHVKnYWqcb6gMghPXXUQ+ACRC66OaY"
+    DOCKER_REGISTRY_SERVER_USERNAME     = azurerm_container_registry.appcr.admin_username
+    DOCKER_REGISTRY_SERVER_PASSWORD     = "LSrXwaiUbuCU5+XHnSQyLt9aeOLaRa/b7XqqsMXg/X+ACRCEoc34"
     WEBSITES_PORT                       = "8080"
   }
   timeouts {}
